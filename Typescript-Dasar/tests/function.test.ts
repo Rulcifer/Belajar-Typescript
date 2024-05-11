da5305679c0babdb0b19931becb61019d8e874ee
Rucifer <sahrulrafi7@gmail.com>
2024-05-11 18:52:31 +0700

    function parameter

--- Typescript-Dasar/tests/function.test.ts
+++ Typescript-Dasar/tests/function.test.ts
@@ -14,4 +14,40 @@ describe('Function', function () {
         printHello("Sahrul");
 
     });
+
+    it('should support default value', function () {
+        function sayHello(name: string = "Guest"): string {
+            return `Hello ${name}`;
+        }
+
+        expect(sayHello()).toBe("Hello Guest");
+        expect(sayHello("Sahrul")).toBe("Hello Sahrul");
+    });
+
+    it('should support parameter', function () {
+        function sum(...values: number[]): number {
+            let total = 0;
+            for (const value of values) {
+                total += value;
+            }
+            return total;
+        }
+
+        expect(sum(1,2,3,4,5)).toBe(15);
+
+    });
+
+    it('should support optional parameter', function () {
+        function sayHello(firstName : string, lastName? : string): string {
+            if(lastName){
+                return `Hello ${firstName} ${lastName}`;
+            } else {
+                return `Hello ${firstName}`;
+            }
+        }
+
+        expect(sayHello("Sahrul")).toBe("Hello Sahrul");
+        expect(sayHello("Sahrul", "Rafi")).toBe("Hello Sahrul Rafi");
+    });
+
 });
