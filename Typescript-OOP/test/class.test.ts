765b4cadefafdc50874e8d8a303be2cc4014bba5
Rucifer <sahrulrafi7@gmail.com>
2024-05-15 17:06:55 +0700

    class

---
+++ Typescript-OOP/test/class.test.ts
@@ -0,0 +1,16 @@
+describe('Class', () => {
+    it('shoul can create class', () => {
+        
+        class Customer {
+
+        }
+
+        class Order {
+            
+        }
+
+        const customer: Customer = new Customer();
+        const order = new Order();
+        
+    });
+});
