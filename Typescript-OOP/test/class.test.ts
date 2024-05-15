3a1c12fdfdb0940d883dfa7762f09d6e3006ce58
Rucifer <sahrulrafi7@gmail.com>
2024-05-15 17:13:05 +0700

    constructor

--- Typescript-OOP/test/class.test.ts
+++ Typescript-OOP/test/class.test.ts
@@ -1,16 +1,23 @@
 describe('Class', () => {
-    it('shoul can create class', () => {
-        
-        class Customer {
 
+        class Customer {
+            constructor() {
+                console.info("Create new customer");
+            }
         }
 
         class Order {
             
         }
 
+        it('should can create class', () => {
         const customer: Customer = new Customer();
         const order = new Order();
-        
+    });
+
+        it('should can create constructors', () => {
+            new Customer();
+            new Customer();
+
     });
 });
