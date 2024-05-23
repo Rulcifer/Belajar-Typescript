dccc990c2f27d6844737b3b229b7215a3df8307b
Rucifer <sahrulrafi7@gmail.com>
2024-05-23 22:00:51 +0700

    type cast

--- Typescript-OOP/test/polymorphism.test.ts
+++ Typescript-OOP/test/polymorphism.test.ts
@@ -14,8 +14,32 @@ describe('Polymorphism', () => {
     }
 
     function sayHello(employee: Employee): void {
-        console.info(`Hello ${employee.name}`);
+        // console.info(`Hello ${employee.name}`);
+
+        if(employee instanceof VicePresident){
+            const vp = employee as VicePresident;
+            console.info(`Hello VP ${vp.name}`);
+        } else if (employee instanceof Manager){
+            const manager = employee as Manager;
+            console.info(`Hello Manager ${manager.name}`);
+        } else {
+            console.info(`Hello Employee ${employee.name}`);
+        }
     }
+
+    //type cast yang salah
+    function sayHelloWrong(employee: Employee): void {
+        if(employee instanceof Manager){
+            const manager = employee as Manager;
+            console.info(`Hello Manager ${manager.name}`);
+        } else if (employee instanceof VicePresident){
+            const vp = employee as VicePresident;
+            console.info(`Hello VP ${vp.name}`);
+        } else {
+            console.info(`Hello Employee ${employee.name}`);
+        }
+    }
+
 
     it('should support', () => {
         let employee : Employee = new Employee("Sahrul");
@@ -33,6 +57,13 @@ describe('Polymorphism', () => {
         sayHello(new Employee("Sahrul"));
         sayHello(new Manager("Rafi"));
         sayHello(new VicePresident("Zul"));
+            
+    });
+
+    it('should support method parameter polymorphism wrong', () => {
+        sayHelloWrong(new Employee("Sahrul"));
+        sayHelloWrong(new Manager("Rafi"));
+        sayHelloWrong(new VicePresident("Zul"));
             
     });
 });
