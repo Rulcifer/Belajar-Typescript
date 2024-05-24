58fd8b4fb0a936115c4dec8b722a36cf2aeacd55
Rucifer <sahrulrafi7@gmail.com>
2024-05-24 19:17:22 +0700

    class relationship

---
+++ Typescript-OOP/test/relationship.test.ts
@@ -0,0 +1,17 @@
+describe('Relationship', () => {
+    class Person {
+        constructor(public name: string) {
+        }
+    }
+
+    class Customer {
+        constructor(public name: string) {
+        }
+    }
+
+    it('should support', () => {
+    const person: Person = new Customer("Sahrul");
+    console.info(person.name);
+    });
+
+});
